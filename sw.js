ae2b7a553b0b956048c70bcd86476b0857d918fa
Gal <Soprgal@gmail.com>
2026-08-11 11:56:44 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-const STATIC_CACHE = "cah-il-static-v34.19.11";
-const RUNTIME_CACHE = "cah-il-runtime-v34.19.11";
-const FONT_CACHE = "cah-il-fonts-v34.19.11";
+const STATIC_CACHE = "cah-il-static-v34.19.14";
+const RUNTIME_CACHE = "cah-il-runtime-v34.19.14";
+const FONT_CACHE = "cah-il-fonts-v34.19.14";
 
 const APP_SHELL = [
   "./",
